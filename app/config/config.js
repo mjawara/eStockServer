37435a46fb6d63ae7da638799fd98368be434b5c
Satit Rianpit <rianpit@gmail.com>
2015-03-06 16:33:39 +0700

    chage default U/P for mysql connection

--- app/config/config.js
+++ app/config/config.js
@@ -9,7 +9,7 @@ module.exports = {
         client: 'mysql',
         connection: {
             host: '127.0.0.1',
-            user: 'root',
+            user: 'estock',
             password: '123456',
             database: 'estock_serv',
             port: 3306
