d78ee772e262744d5b8bd611194605a9083466e4
Satit Rianpit <rianpit@gmail.com>
2015-03-06 15:29:52 +0700

    Change default value from number to string

--- public/js/purchases/NewController.js
+++ public/js/purchases/NewController.js
@@ -35,7 +35,7 @@ App.controller('NewController', function ($scope, $location, NewService, LxDialo
     };
 
     $scope.addItem = function (code) {
-        var qty = prompt('ระบุจำนวนที่ต้องการสั่ง', 0);
+        var qty = prompt('ระบุจำนวนที่ต้องการสั่ง', '0');
 
         if (qty > 0) {
             var idx = _.findIndex($scope.products, {
